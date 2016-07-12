a695951f748d2ee52aa0cac74399ab574ffcbd6f
Nicolas Byl <nico@nicolas-byl.eu>
2016-07-12 22:29:03 +0200

    docker-machine support works

--- src/main/groovy/com/github/nbyl/gradle/dockercompose/tasks/DockerComposeBaseTask.groovy
+++ src/main/groovy/com/github/nbyl/gradle/dockercompose/tasks/DockerComposeBaseTask.groovy
@@ -1,5 +1,6 @@
 package com.github.nbyl.gradle.dockercompose.tasks
 
+import com.github.nbyl.gradle.dockercompose.machine.DockerMachineEnvironmentReader
 import com.github.nbyl.gradle.dockercompose.runner.DockerComposeRunner
 import org.gradle.api.DefaultTask
 
@@ -7,13 +8,22 @@ abstract class DockerComposeBaseTask extends DefaultTask {
 
     def String composeFile
 
+    def String dockerMachineEnvironment
+
     def DockerComposeRunner runner
+
+    def DockerMachineEnvironmentReader environmentReader
 
     def getRunner() {
         def DockerComposeRunner runner = (this.runner == null) ? new DockerComposeRunner() : this.runner
 
         if (composeFile) {
             runner.withComposeFile(composeFile)
+        }
+
+        if (dockerMachineEnvironment) {
+            def DockerMachineEnvironmentReader environmentReader = (this.environmentReader == null) ? new DockerMachineEnvironmentReader(project: project) : this.environmentReader
+            runner.withEnvironmentVariables(environmentReader.readEnvironment(dockerMachineEnvironment))
         }
 
         return runner.withProject(project)
